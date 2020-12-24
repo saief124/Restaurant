8d50703f6d2491e638138ac6b89ab0921cd9ad3f
jpeterson14 <jpeterson0514@gmail.com>
2020-12-24 14:47:03 -0600

    basic routes done

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :item_orders
-  resources :carts
-  resources :customers
-  resources :menu_items
-  resources :stores
+  # resources :item_orders
+  resources :carts, only: [:show]
+  resources :customers, except: [:new, :destroy, :edit]
+  resources :menu_items, only: [:index, :show]
+  resources :stores, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
