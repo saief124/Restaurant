331db4fe5599316d05a2e6c413c6a10c77c8ffb4
saief124 <saiefsayed123@hotmail.com>
2021-01-03 19:06:37 -0600

    slight stylistic change

--- frontend/my-project-client/src/components/MenuItem.js
+++ frontend/my-project-client/src/components/MenuItem.js
@@ -3,8 +3,11 @@ import React from 'react'
 export default function MenuItem(props) {
     return (
         <div>
-            {props.item.name} {props.item.price}   {props.item.description}<br></br>
+            <br></br>
+            <p>{props.item.name} ---  ${props.item.price}</p>
+            <p>{props.item.description}</p>
             <button onClick={()=>props.handleOrders(props.item)}>Add Item+</button>
+            
         </div>
     )
 }
